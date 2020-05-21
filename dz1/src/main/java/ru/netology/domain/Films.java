6ecd77be6d1a05788f8aba426ffdb590f9461ee3
Odium-Mundi <61995344+Odium-Mundi@users.noreply.github.com>
2020-05-21 23:20:20 +0300

    Update Films.java

--- dz1/src/main/java/ru/netology/domain/Films.java
+++ dz1/src/main/java/ru/netology/domain/Films.java
@@ -1,13 +1,1 @@
-package ru.netology.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
-
-public class Films {
-    private String filmName;
-}
